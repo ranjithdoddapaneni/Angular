9d26416303b2b5cb12a4f6e1cff704c0e8ef9564
RanjithDoddapaneni <Ranjith.Doddapaneni@gmail.com>
2018-06-10 12:48:34 -0400

    DataTable sample

---
+++ AngularApp/AngularApp/AngularScripts/Home.js
@@ -0,0 +1,12 @@
+﻿var myHome = angular.module("MyHome", [])
+    .controller("MyHomeController", function ($scope) {
+        var employees = [
+            { FirstName: "Ranjith", LastName: "Doddapaneni", DateofBirth: new Date("May 18, 1986"), Gender: "Male", Salary: 100000, Designation: "Application Architecht" },
+            { FirstName: "Monika", LastName: "Doddapaneni", DateofBirth: new Date("Feb 20, 1991"), Gender: "Female", Salary: 100000, Designation: "Software Developer" },
+            { FirstName: "Bhanu", LastName: "Kammakomati", DateofBirth: new Date("May 18, 1985"), Gender: "Male", Salary: 100000, Designation: "Solution Architecht" },
+            { FirstName: "Rakesh", LastName: "Bhumireddy", DateofBirth: new Date("May 18, 1986"), Gender: "Male", Salary: 100000, Designation: "SOA Architect" },
+            { FirstName: "Rajni", LastName: "Bhumireddy", DateofBirth: new Date("May 18, 1987"), Gender: "Female", Salary: 100000, Designation: "Middleware  Architecht" },
+            { FirstName: "Radhika", LastName: "Kammakokmati", DateofBirth: new Date("May 18, 1985"), Gender: "Female", Salary: 100000, Designation: "Application Architecht" }
+        ];
+        $scope.Employees = employees;
+    });
